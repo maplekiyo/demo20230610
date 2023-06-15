e30c30e38980196f30b9713a01cddb3287360b91
Koichi Hoshina <maplekiyo@yahoo.co.jp>
2023-06-15 16:43:40 +0900

    Hoge-Foo-Bar

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -8,8 +8,8 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello Bar!" );
-        System.out.println( "Hello Foo!" );
         System.out.println( "Hello Hoge!" );
+        System.out.println( "Hello Foo!" );
+        System.out.println( "Hello Bar!" );
     }
 }
